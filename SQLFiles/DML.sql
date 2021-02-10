0b02e48f9f659d0f449b8ec40513ea3b45332ad4
p0pkern <p0pkerncs@gmail.com>
2021-02-09 19:19:59 -0800

    Completed features and bugs for DML.sql file

--- SQLFiles/DML.sql
+++ SQLFiles/DML.sql
@@ -31,4 +31,40 @@ INSERT INTO employees_teams (employee, team) VALUES (:employee_id_input, :team_i
 --Delete Team member
 DELETE FROM employees_teams WHERE employee = :employee_id_from_team_members_list AND team = team_id_from_the_team_list
 
+-- Get all bugs in the list
+SELECT * FROM Bugs; 
 
+-- Get all bugs that are resolved
+SELECT * FROM Bugs WHERE complete = 1;
+
+-- GET all bugs that are unresolved
+SELECT * FROM Bugs WHERE complete = 0;
+
+-- Add new bug
+INSERT INTO Bugs (describe, urgency, complete, employee) VALUES (:description_of_issue, :urgency_level_number, :boolean_of_complete_default_0, :employee_id_from_employee_table_or_null);
+
+-- Update bug
+UPDATE Bugs SET describe = :new_description, urgency = :new_urgency, employee = :new_employee_or_null WHERE bug_id = :bug_id_from_list;
+
+-- Complete bug repair
+UPDATE Bugs SET complete = 1 WHERE bug_id = :bug_id_from_list;
+
+-- Delete bug
+DELETE FROM Bugs WHERE bug_id = :bug_id_of_bug_to_be_deleted;
+
+-- Get all features with team name or team name null
+SELECT * FROM Features 
+LEFT JOIN Teams.team_name
+ON Features.team = Teams.team_id;
+
+-- Add new features
+INSERT INTO Features (describe, deadline, condition, team) VALUES (:description_of_feature, :date_to_complete, :current_status_of_project, :team_assigned_defaults_to_null);
+
+-- Add team to feature
+INSERT INTO Features (team) VALUES ((SELECT team_id FROM Teams WHERE team_id = :team_id_to_assign));
+
+-- Update feature
+UPDATE Features SET describe = :new_description, deadline = :new_deadline, condition = :new_condition, team = :new_team_or_null;
+
+-- Delete feature
+DELETE FROM Features WHERE feature_id = :feature_id_to_be_deleted;
