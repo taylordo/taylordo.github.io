94cdb4eadab162611d0512343b984ebcd3ee031a
p0pkern <p0pkerncs@gmail.com>
2021-02-21 14:25:18 -0800

    did work on bugs pages, it will now submit and write to database. Will need to revisit how we want to set up pages and possibly change database.js to bugs_server.js

--- database/database.js
+++ database/database.js
@@ -6,10 +6,11 @@ var app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: false}));
 app.set('port', 4756);
-app.use("/public_html", express.static('../../public_html/'));
+app.use("/public_html", express.static('../../../public_html/'));
 app.use(CORS())
 
-const getAllQuery = "SELECT * FROM Bugs;"
+const getAllQuery = "SELECT * FROM Bugs;";
+const insertQuery = "INSERT INTO Bugs (`description`, `urgency`, `complete`, `employee`) VALUES (?, ?, ?, ?)";
 
 const getAllData = (res) => {
   mysql.pool.query(getAllQuery, (err, rows, fields) => {
@@ -34,6 +35,27 @@ app.get('/',function(req,res,next){
     });
   });
 
+  //Insert New Bug
+app.post('/',function(req,res,next){
+
+  console.log(req.body)
+
+  //var context = {};
+  
+  var {new_description_input, new_urgency_input, new_completion_input, new_employee_input} = req.body;
+  console.log(new_description_input)
+  console.log(new_urgency_input)
+  console.log(new_completion_input)
+  console.log(new_employee_input)
+  mysql.pool.query(insertQuery, [new_description_input, new_urgency_input, new_completion_input, new_employee_input], function(err, result){
+    if(err){
+      next(err);
+      return;
+    }
+    getAllData(res);
+  });
+  
+});
 
 app.use(function(req,res){
     res.status(404);
