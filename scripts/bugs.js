9cafe0d6263d5dcb993737591e028848f649508c
p0pkern <p0pkerncs@gmail.com>
2021-02-23 18:21:11 -0800

    added ACTUAL files

--- scripts/bugs.js
+++ scripts/bugs.js
@@ -1,35 +1,51 @@
-
 /* New bug submission data harvested from form */
 
-let getBugData = function() {
-const description = document.getElementById('newdescription').value;
-const urgency = document.getElementById('newurgency').value;
-const completion = document.getElementById('newcompletion').value;
-const employee = document.getElementById('newemployee').value;
+let getNewBugData = function() {
+    const new_description_input = document.getElementById('new_description_input').value;
+    const new_urgency_input = document.getElementById('new_urgency_input').value;
+    const new_completion_input = document.getElementById('new_completion_input').value;
+    const new_employee_input = document.getElementById('new_employee_input').value;
+    
+    let new_bug_data = {};
+    new_bug_data["new_description_input"] = new_description_input;
+    new_bug_data["new_urgency_input"] = new_urgency_input;
+    new_bug_data["new_completion_input"] = new_completion_input;
+    new_bug_data["new_employee_input"] = new_employee_input;
+    
+    console.log(new_bug_data)
+    return new_bug_data;
+}
 
-let bugSubData = {};
-bugSubData["description"] = description;
-bugSubData["urgency"] = urgency;
-bugSubData["completion"] = completion;
-bugSubData["employee"] = employee;
+function deleteTable(){
+    //deletes the table
+    const rootParent = document.getElementById('featured_bugs');
+    
+    while(rootParent.firstElementChild != null){
+        rootParent.removeChild(rootParent.firstElementChild)
+    }
 
-console.log(bugSubData);
-
-return bugSubData
 }
 
 /* Generate Table of pre-existing bugs onto  */
 
-let attachBugCard = function (dataSet) {
+let attachBugCard = function (dataSet, empData) {
     const rootParent = document.getElementById('featured-bugs');
     for (let i = 0; i < dataSet.length; i++) {
-        rootParent.appendChild(generateBugCard(dataSet[i]));
+        rootParent.appendChild(generateBugCard(dataSet[i], empData));
     }
 }
 
-let generateBugCard = function (dataSet) {
+let generateBugCard = function (dataSet, empData) {
     const form = document.createElement('form');
     const fieldset = document.createElement('fieldset');
+    let getName;
+    let dataEmpNumber = dataSet.employee;
+    for (i=0; i < empData.length; i++) {
+        if (empData[i].employee_id == dataEmpNumber) {
+            getName = `${empData[i].first_name} ${empData[i].last_name}`;
+            break;
+        }
+    }
     const br = document.createElement('br');
     const br1 = document.createElement('br')
     /* Add content to card */
@@ -37,7 +53,7 @@ let generateBugCard = function (dataSet) {
     fieldset.appendChild(generateTextArea(dataSet));
     fieldset.appendChild(br);
     fieldset.appendChild(generateUrgency(dataSet));
-    fieldset.appendChild(generateName(dataSet));
+    fieldset.appendChild(generateName(dataSet, getName));
     fieldset.appendChild(br1)
     fieldset.appendChild(generateUpdate(dataSet));
     fieldset.appendChild(generateDelete(dataSet));
@@ -61,7 +77,7 @@ let generateTextArea = function (dataSet) {
     textArea.setAttribute('name', `description${dataSet.id}`);
     textArea.setAttribute('rows', 4);
     textArea.setAttribute('columns', 150);
-    textArea.setAttribute('id', 'description');
+    textArea.setAttribute('bug_id', 'description');
     textArea.textContent = dataSet.description;
     textArea.disabled = true;
     return textArea;
@@ -75,7 +91,7 @@ let generateHeader = function (dataSet) {
         resolveStatus = 'Resolved'
         header.style.textDecoration = "line-through";
     }
-    header.textContent = "Bug" + " " + dataSet.id + " " + resolveStatus;
+    header.textContent = "Bug" + " " + dataSet.bug_id + " " + resolveStatus;
     return header;
 }
 
@@ -90,12 +106,12 @@ let generateUrgency = function (dataSet) {
     return label;
 }
 
-let generateName = function (dataSet) {
+let generateName = function (dataSet, empName) {
     const input = document.createElement('input');
     const employeeNum = `employee${dataSet.id}`
     input.setAttribute('type', 'text');
     input.setAttribute('id', employeeNum);
-    input.setAttribute('value', dataSet.employee);
+    input.setAttribute('value', empName);
     input.disabled = true;
     const label = generateLabel(employeeNum, ' Employee Assigned: ');
     label.appendChild(input);
@@ -126,41 +142,64 @@ let generateResolved = function (dataSet) {
     return input;
 }
 
-/* TEST Table Data */
-const testData = [{'id': '001', 
-                   'description': 'This is the first item in the lise',
-                   'urgency': '1',
-                   'employee': 'Phil Mckracken',
-                   'resolved': false
-                   },
-                   {'id': '002', 
-                    'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, sed accusantium. Porro, vero aliquam consectetur reiciendis, quas, ab architecto tempore tempora voluptates illo reprehenderit in vel temporibus sequi facilis inventore?',
-                    'urgency': '2',
-                    'employee': 'Bill Braskerman',
-                    'resolved': true
-                   },
-                   {'id': '003',
-                    'description': 'Backend Will not connect to MariaDB due to CORS error, I am currently too dumb to figure it out.',
-                    'urgency': '3',
-                    'employee': 'Don',
-                    'resolved': false}]
-
-let baseUrl = "https://flip2.engr.oregonstate.edu:4756/"
-
-const loadContent = function(event){
-    var req = new XMLHttpRequest();
-    req.open('GET', baseUrl, false);
-    req.send(null);
-    if (req.status >=200 && req.status < 400) {
-    var response = JSON.parse(req.responseText);
-    } else {
-        console.log(req.statusText);
+let addEmployeeDropDown = function (dataSet) {
+    console.log(dataSet)
+    const empInput = document.getElementById('new_employee_input');
+    for (i = 0; i < dataSet.length; i++) {
+        let dataPoint = document.createElement('option');
+        dataPoint.setAttribute('value', dataSet[i].employee_id);
+        dataPoint.textContent = `${dataSet[i].first_name} ${dataSet[i].last_name}`;
+        empInput.add(dataPoint)
     }
-    console.log(response);
-    // makeTable(response);
+}
+
+let baseUrl = "http://flip2.engr.oregonstate.edu:4756/"
+
+//Initial Display
+document.addEventListener('DOMContentLoaded', function(event) {
+    var req = new XMLHttpRequest();
+    req.open('GET', baseUrl, true);
+    req.send(null);
+    
+    req.addEventListener('load', function(){
+        if (req.status >=200 && req.status < 400) {
+            var response = JSON.parse(req.responseText);
+        
+        } else {
+            console.log(req.statusText);
+        }
+
+        console.log(response);
+        addEmployeeDropDown(response['employees'])
+        attachBugCard(response['rows'], response['employees'])
+        event.preventDefault();
+    })
+
+});
+
+//Event listener for new bugs button
+document.getElementById('new_submit').addEventListener('click', function(event){
+    var req = new XMLHttpRequest();
+    req.open("POST", baseUrl, true);
+    req.setRequestHeader('Content-Type', 'application/json');
+    
+    req.addEventListener('load',function(){
+        if(req.status >=200 && req.status < 400){
+            var response = JSON.parse(req.responseText)
+            
+            // deleteTable()
+            // attachBugCard(response['rows'])
+            console.log(response)
+        }
+        else{
+            console.log(req.statusText)
+        }
+    });
+    
+    new_bug_data = getNewBugData()
+    console.log(new_bug_data)
+    req.send(JSON.stringify(new_bug_data));
+    // document.getElementById("new_submit").reset();
     // event.preventDefault();
-};
 
-document.addEventListener('DOMContentLoaded', loadContent);
-
-document.getElementById('newsubmit').addEventListener('click', getBugData);
+  })
--- scripts/features.js
+++ scripts/features.js
@@ -1,10 +1,10 @@
 /* Retrieve data for new feature submission */
 
 let getFeatData = function() {
-    const description = document.getElementById('newdescription').value;
-    const date = document.getElementById('newdate').value;
-    const status = document.getElementById('newstatus').value;
-    const team = document.getElementById('newteam').value;
+    const description = document.getElementById('new_description_input').value;
+    const date = document.getElementById('new_date_input').value;
+    const status = document.getElementById('new_status_input').value;
+    const team = document.getElementById('new_team_input').value;
     
     let featSubData = {};
     featSubData["description"] = description;
@@ -19,14 +19,14 @@ let getFeatData = function() {
 
 /* Generate Table of pre-existing Features onto  */
 
-let attachFeatCard = function (dataSet) {
+let attachFeatCard = function (dataSet, team) {
     const rootParent = document.getElementById('featured-features');
     for (let i = 0; i < dataSet.length; i++) {
-        rootParent.appendChild(generateFeatCard(dataSet[i]));
+        rootParent.appendChild(generateFeatCard(dataSet[i], team));
     }
 }
 
-let generateFeatCard = function (dataSet) {
+let generateFeatCard = function (dataSet, team) {
     const form = document.createElement('form');
     const fieldset = document.createElement('fieldset');
     const br = document.createElement('br');
@@ -36,7 +36,7 @@ let generateFeatCard = function (dataSet) {
     fieldset.appendChild(br);
     fieldset.appendChild(generateDate(dataSet));
     fieldset.appendChild(generateStatus(dataSet));
-    fieldset.appendChild(generateTeam(dataSet));
+    fieldset.appendChild(generateTeam(dataSet, team));
     fieldset.appendChild(generateUpdate(dataSet));
     fieldset.appendChild(generateDelete(dataSet));
     
@@ -49,7 +49,7 @@ let generateFeatCard = function (dataSet) {
 
 let generateHeader = function (dataSet) {
     const header = document.createElement('h3');
-    header.textContent = "Feature" + " " + dataSet.id;
+    header.textContent = "Feature" + " " + dataSet.feature_id;
     return header;
 }
 
@@ -62,7 +62,7 @@ let generateLabel = function (name, content) {
 
 let generateTextArea = function (dataSet) {
     const textArea = document.createElement('textarea');
-    textArea.setAttribute('name', `description${dataSet.id}`);
+    textArea.setAttribute('name', `description${dataSet.feature_id}`);
     textArea.setAttribute('rows', 4);
     textArea.setAttribute('columns', 150);
     textArea.setAttribute('id', 'description');
@@ -74,30 +74,70 @@ let generateTextArea = function (dataSet) {
 let generateDate = function (dataSet) {
     const input = document.createElement('input');
     input.setAttribute('type', 'date');
-    input.setAttribute('id', `date${dataSet.id}`);
-    input.setAttribute('value', dataSet.date);
+    input.setAttribute('id', `date${dataSet.feature_id}`);
+    let newDate = sliceDate(dataSet.deadline)
+    input.setAttribute('value', newDate);
     input.disabled = true;
-    const label = generateLabel(`date${dataSet.id}`, ' Deadline: ')
+    const label = generateLabel(`date${dataSet.feature_id}`, ' Deadline: ')
     label.appendChild(input);
     return label;
+}
+
+let sliceDate = function (date) {
+    const newDate = date.slice(0,10);
+    return newDate
 }
 
 let generateStatus = function (dataSet) {
     let input = document.createElement('input');
     input.setAttribute('type', 'number');
-    input.setAttribute('id', `status${dataSet.id}`)
+    input.setAttribute('id', `status${dataSet.feature_id}`)
     input.setAttribute('value', dataSet.status);
-    const label = generateLabel(`status${dataSet.id}`, 'Status: ');
+    input.disabled = true;
+    const label = generateLabel(`status${dataSet.feature_id}`, 'Status: ');
     label.appendChild(input);
     return label;
 }
 
-let generateTeam = function (dataSet) {
-    let input = document.createElement('input');
+let generateTeam = function (dataSet, team) {
+    let input = document.createElement('select');
+    let nullOption = document.createElement('option');
+    nullOption.textContent = 'No Team';
+    nullOption.setAttribute('value', null);
+    if (dataSet.team === null) {
+        input.add(nullOption);
+        for(i = 0; i < team.length; i++) {
+            let option = document.createElement('option');
+            option.setAttribute('id', `team${team[i].team_id}`)
+            option.textContent = team[i].team_name
+            input.add(option)
+        } 
+    } else {
+        for(i = 0; i < team.length; i++) {
+            if(dataSet.team === team[i].team_id) {
+                let option = document.createElement('option');
+                option.setAttribute('id', `team${team[i].team_id}`)
+                option.textContent = team[i].team_name
+                input.add(option)
+                break;
+            }
+        }
+        input.add(nullOption);
+        for(j = 0; j < team.length; j++) {
+            if(dataSet.team !== team[j].team_id) {
+                let option = document.createElement('option');
+                option.setAttribute('id', `team${team[j].team_id}`)
+                option.textContent = team[j].team_name
+                input.add(option)
+            }
+        }
+    }
     input.setAttribute('type', 'text');
-    input.setAttribute('id', `team${dataSet.id}`)
+    input.setAttribute('id', `team${dataSet.feature_id}`)
+    // Add select if no team
     input.setAttribute('value', dataSet.team);
-    const label = generateLabel(`team${dataSet.id}`, 'Team Assigned: ');
+    input.disabled = true;
+    const label = generateLabel(`team${dataSet.feature_id}`, 'Team Assigned: ');
     label.appendChild(input);
     return label;
 }
@@ -105,7 +145,7 @@ let generateTeam = function (dataSet) {
 let generateUpdate = function (dataSet) {
     let input = document.createElement('input');
     input.setAttribute('type', 'submit');
-    input.setAttribute('id', `edit${dataSet.id}`);
+    input.setAttribute('id', `edit${dataSet.feature_id}`);
     input.setAttribute('value', 'Edit');
     return input;
 }
@@ -113,30 +153,79 @@ let generateUpdate = function (dataSet) {
 let generateDelete = function (dataSet) {
     let input = document.createElement('input');
     input.setAttribute('type', 'submit');
-    input.setAttribute('id', `delete${dataSet.id}`);
+    input.setAttribute('id', `delete${dataSet.feature_id}`);
     input.setAttribute('value', 'Delete');
     return input;
 }
 
+let generateTeamChoice = function(dataSet) {
+    const teamSubmit = document.getElementById('new_team_input');
+    for (i = 0; i < dataSet.length; i++) {
+        let option = document.createElement('option')
+        option.textContent = `${dataSet[i].team_name}`;
+        option.setAttribute('value', dataSet[i].team_id);
+        teamSubmit.add(option)
+    }
+}
 
-/* TEST Table Data */
-const testData = [{'id': '001', 
-                   'description': 'This is the first item in the lise',
-                   'date': '2012-10-02',
-                   'status': 1,
-                   'team': 'Front End'
-                   },
-                   {'id': '002', 
-                   'description': 'Another item',
-                   'date': '2012-11-12',
-                   'status': 1,
-                   'team': 'Anything Goes'
-                   },
-                   {'id': '003', 
-                   'description': 'Add a nice layout',
-                   'date': '2015-02-02',
-                   'status': 0,
-                   'team': 'Back End'}]
+let deleteTable = function() {
+    const features = document.getElementById('featured-features');
 
-document.addEventListener('DOMContentLoaded', attachFeatCard(testData));
-document.getElementById('newsubmit').addEventListener('click', getFeatData);
+    while(features.firstElementChild != null){
+        features.removeChild(features.firstElementChild)
+    }
+}
+
+let baseUrl = "http://flip2.engr.oregonstate.edu:4756/"
+
+//Initial Display
+document.addEventListener('DOMContentLoaded', function(event) {
+var req = new XMLHttpRequest();
+req.open('GET', baseUrl, true);
+req.send(null);
+
+req.addEventListener('load', function(){
+    if (req.status >=200 && req.status < 400) {
+        var response = JSON.parse(req.responseText);
+    
+    } else {
+        console.log(req.statusText);
+    }
+
+    deleteTable()
+    generateTeamChoice(response['team']);
+    attachFeatCard(response['rows'], response['team']);
+    event.preventDefault();
+})
+
+});
+
+// Event listener for new bugs button
+document.getElementById('new_submit_input').addEventListener('click', function(event){
+var req = new XMLHttpRequest();
+req.open("POST", baseUrl, true);
+req.setRequestHeader('Content-Type', 'application/json');
+
+req.addEventListener('load',function(){
+    if(req.status >=200 && req.status < 400){
+        var response = JSON.parse(req.responseText)
+        
+        console.log(response)
+        // deleteTable()
+        // generateTeamChoice(response['team']);
+        // attachFeatCard(response['rows'], response['team']);
+        location.reload();
+        
+    }
+    else{
+        console.log(req.statusText)
+    }
+});
+
+new_feat_data = getFeatData()
+console.log(new_feat_data)
+req.send(JSON.stringify(new_feat_data));
+// document.getElementById("new_submit").reset();
+// event.preventDefault();
+
+})
